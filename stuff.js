f689d095ce8b857c4a954b85a0ab427c147d7e46
Marc Majcher <majcher@gmail.com>
2017-01-26 14:27:31 -0600

    claaaackd

--- stuff.js
+++ stuff.js
@@ -4,9 +4,21 @@
   console.log('I LIVE');
   const app = angular.module('demo', []);
 
-  const bookFun = () => {
+  const bookFun = function bookFun($http) {
+    const vm = this;
 
+    vm.clickMe = () => {
+      console.log('CLAAACKD!111');
+      $http.get('/data.json')
+        .then((response) => {
+          console.log('DAAAAATAAA');
+          console.log(response.data);
+          vm.books = response.data;
+        });
+    };
   };
+
+  bookFun.$inject = ['$http'];
 
   app.component('bookGetter', {
     controller: bookFun,
